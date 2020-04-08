211c9df7f05d793314cad5232c9a0c024d68cd82
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-04-08 17:12:20 +0800

    Update SendDataAsyncSample.py

--- SendDataAsyncSample/SendDataAsyncSample.py
+++ SendDataAsyncSample/SendDataAsyncSample.py
@@ -1,26 +1,15 @@
-# Copyright 2017, Digi International Inc.
-#
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# Asynchronous operation
+# Transmitting data asynchronously means that your application does not block during the transmit process. However, you cannot ensure that the data was successfully sent to the remote device.
 
 from digi.xbee.devices import XBeeDevice
 
 # TODO: Replace with the serial port where your local module is connected to.
-PORT = "COM1"
+PORT = "COM5"
 # TODO: Replace with the baud rate of your local module.
 BAUD_RATE = 9600
 
 DATA_TO_SEND = "Hello XBee!"
-REMOTE_NODE_ID = "REMOTE"
+REMOTE_NODE_ID = "Test"
 
 
 def main():
